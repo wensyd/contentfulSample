27c1c62d8af2f11ac1879c310952366797df83fe
wensydesousa <wensys013@gmail.com>
2022-02-11 20:14:43 -0800

    changes made, Thank you Gandalfgit add .

--- src/blogCards.js
+++ src/blogCards.js
@@ -4,14 +4,16 @@ import {Link} from 'react-router-dom'
 const BlogCard = ({blogPost}) => {
 
 return (
-<div className="card" style="width:400px">
-  <img className="card-img-top" src={blogPost.heroImage.file.url} alt="Card image"/>
+    <div class="container">
+<div className="card" style={{width: '400px'}}>
+  <img className="card-img-top" src={blogPost.heroImage.fields.file.url} alt="Card image"/>
   <div className="card-body">
     <h4 className="card-title">{blogPost.title}</h4>
     <p className="card-text">{blogPost.description}</p>
     <p className="card-text">{blogPost.body}</p>
     <Link to={blogPost.slug} className="btn btn-primary">Read More</Link>
   </div>
+</div>
 </div>
 
 )}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {BrowserRouter as Router} from "react-router-dom"
+
+
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>,
   document.getElementById("root")
 );
 
